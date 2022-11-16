bf539745dba926ad4dfa5ea8fb65fd783eb9b8f9
Macey Cohn <mcohn@vt.edu>
2022-11-15 23:49:47 -0500

    Added Jerald's redirection script

--- Assets/Scripts/Redirection.cs
+++ Assets/Scripts/Redirection.cs
@@ -4,20 +4,35 @@ using UnityEngine;
 
 public class Redirection : MonoBehaviour
 {
-    private float oldPos;
+    public GameObject trackingSpace;
+    public float translationGain = 1.0f;
+    
+    private Vector3 oldPhysicalPos;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        oldPos = transform.position.x;
+        oldPhysicalPos = transform.localPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (oldPos != transform.position.x) {
-            transform.position = new Vector3(transform.position.x * 3, transform.position.y, transform.position.z);
-            oldPos = transform.position.x;
-        }
+        // Get the change in physical position
+        Vector3 deltaPhysicalPos = transform.localPosition - oldPhysicalPos;
+
+        // Calculate amount of injected translation
+        Vector3 injectedTranslation = deltaPhysicalPos * (translationGain - 1.0f);
+
+        // Move tracking space
+        trackingSpace.transform.Translate(injectedTranslation);
+
+        // Update old pos
+        oldPhysicalPos = transform.localPosition;
+
+        
+        
+        // get eulerRotation.y for rotation
     }
 }
